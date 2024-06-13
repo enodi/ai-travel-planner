adc3694b9273bb5982434aa464e0ae2ffef2ae45
Enodi Audu <enodiaudu5@gmail.com>
2024-06-13 20:38:39 +0100

    design header component

---
+++ src/components/common/header/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <nav className="p-4">
+      <h1 className="text-2xl text-black font-bold text-center">
+        AI Travel Planner
+      </h1>
+    </nav>
+  );
+}
+
+export default Header;
