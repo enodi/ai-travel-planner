0dc31cc42e516971cfa350f042530a5a34a68ccf
Enodi Audu <enodiaudu5@gmail.com>
2024-07-02 15:12:41 +0100

    add switch button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "@chakra-ui/react";
 
 import Layout from "@/src/components/common/layout/Layout";
 import Itinerary from "@/src/components/itinerary/Itinerary";
@@ -8,11 +9,18 @@ import TopSection from "@/src/components/top-section/TopSection";
 import PopularItinerary from "@/src/components/popular-itinerary/PopularItinerary";
 
 export default function Page() {
-  const [hasFetchItinerary, setHasFetchItinerary] = useState(true);
+  const [hasFetchItinerary, setHasFetchItinerary] = useState(false);
+
+  const renderButton = () => (
+    <Button onClick={() => setHasFetchItinerary(!hasFetchItinerary)}>
+      Switch
+    </Button>
+  );
 
   if (!hasFetchItinerary) {
     return (
       <Layout>
+        {renderButton()}
         <TopSection />
         <PopularItinerary />
       </Layout>
@@ -21,6 +29,7 @@ export default function Page() {
 
   return (
     <Layout>
+      {renderButton()}
       <section className="my-10">
         <Itinerary />
       </section>
--- src/lib/supabase/createSupabaseClient.ts
+++ src/lib/supabase/createSupabaseClient.ts
@@ -4,17 +4,17 @@ import { Database } from "@/types.gen"
 
 const env = z
   .object({
-    NEXT_PUBLIC_SUPABASE_URL: z.string(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    SUPABASE_URL: z.string(),
+    SUPABASE_ANON_KEY: z.string(),
   })
   .parse({
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   })
 
 export function createSupabaseClient() {
   return createClient<Database>(
-    env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    env.SUPABASE_URL,
+    env.SUPABASE_ANON_KEY,
   )
 }
